a06c00063cb336577c2e0d2c8d033bfb8636bc2d
pengyong14 <pengyong1@jd.com>
2017-02-10 17:18:00 +0800

    预å本地½图片

--- pages/game/index.js
+++ pages/game/index.js
@@ -19,7 +19,7 @@ Page({
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    this.preloadImgs();
+    this.showStage();
   },
   onReady: function () {
     // 页面渲染完成
@@ -47,71 +47,72 @@ Page({
       bu: '../images/bu.png',
       buWin: '../images/buWin.png',
       buLose: '../images/buLose.png',
-      card1: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_1.jpg',
-      card2: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_2.jpg',
-      card3: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_3.jpg',
-      card4: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_4.jpg',
-      card5: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_5.jpg',
-      card6: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_6.jpg',
-      card7: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_7.jpg',
-      card8: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_8.jpg',
-      card9: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_9.jpg',
-      card10: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_10.jpg',
-      card11: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_11.jpg',
-      card12: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_12.jpg',
-      card13: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_13.jpg',
-      card14: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_14.jpg',
-      card15: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_15.jpg',
-      card16: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_16.jpg',
-      card17: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_17.jpg',
-      card18: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_18.jpg',
-      card19: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_19.jpg',
-      card20: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_20.jpg',
-      card21: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_21.jpg',
-      card22: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_22.jpg',
-      card23: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_23.jpg',
-      card24: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_24.jpg',
-      card25: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_25.jpg',
-      card26: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_26.jpg',
-      card27: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_27.jpg',
-      card28: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_28.jpg',
-      card29: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_29.jpg',
-      card30: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_30.jpg',
-      card31: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_31.jpg',
-      card32: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_32.jpg',
-      card33: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_33.jpg',
-      card34: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_34.jpg',
-      card35: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_35.jpg',
-      card36: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_36.jpg',
-      card37: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_37.jpg',
-      card38: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_38.jpg',
-      card39: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_39.jpg',
-      card40: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_40.jpg',
-      card41: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_41.jpg',
-      card42: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_42.jpg',
-      card43: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_43.jpg',
-      card44: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_44.jpg',
-      card45: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_45.jpg',
-      card46: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_46.jpg',
-      card47: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_47.jpg',
-      card48: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_48.jpg',
-      card49: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_49.jpg',
-      card50: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_50.jpg',
-      card51: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_51.jpg',
-      card52: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_52.jpg',
-      card53: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_53.jpg',
-      card54: 'https://raw.githubusercontent.com/51anygo/anygo/master/puke_images/veryhuo.com_pkp_54.jpg'
+      card1: '../images/puke_1.jpg',
+      card2: '../images/puke_2.jpg',
+      card3: '../images/puke_3.jpg',
+      card4: '../images/puke_4.jpg',
+      card5: '../images/puke_5.jpg',
+      card6: '../images/puke_6.jpg',
+      card7: '../images/puke_7.jpg',
+      card8: '../images/puke_8.jpg',
+      card9: '../images/puke_9.jpg',
+      card10: '../images/puke_10.jpg',
+      card11: '../images/puke_11.jpg',
+      card12: '../images/puke_12.jpg',
+      card13: '../images/puke_13.jpg',
+      card14: '../images/puke_14.jpg',
+      card15: '../images/puke_15.jpg',
+      card16: '../images/puke_16.jpg',
+      card17: '../images/puke_17.jpg',
+      card18: '../images/puke_18.jpg',
+      card19: '../images/puke_19.jpg',
+      card20: '../images/puke_20.jpg',
+      card21: '../images/puke_21.jpg',
+      card22: '../images/puke_22.jpg',
+      card23: '../images/puke_23.jpg',
+      card24: '../images/puke_24.jpg',
+      card25: '../images/puke_25.jpg',
+      card26: '../images/puke_26.jpg',
+      card27: '../images/puke_27.jpg',
+      card28: '../images/puke_28.jpg',
+      card29: '../images/puke_29.jpg',
+      card30: '../images/puke_30.jpg',
+      card31: '../images/puke_31.jpg',
+      card32: '../images/puke_32.jpg',
+      card33: '../images/puke_33.jpg',
+      card34: '../images/puke_34.jpg',
+      card35: '../images/puke_35.jpg',
+      card36: '../images/puke_36.jpg',
+      card37: '../images/puke_37.jpg',
+      card38: '../images/puke_38.jpg',
+      card39: '../images/puke_39.jpg',
+      card40: '../images/puke_40.jpg',
+      card41: '../images/puke_41.jpg',
+      card42: '../images/puke_42.jpg',
+      card43: '../images/puke_43.jpg',
+      card44: '../images/puke_44.jpg',
+      card45: '../images/puke_45.jpg',
+      card46: '../images/puke_46.jpg',
+      card47: '../images/puke_47.jpg',
+      card48: '../images/puke_48.jpg',
+      card49: '../images/puke_49.jpg',
+      card50: '../images/puke_50.jpg',
+      card51: '../images/puke_51.jpg',
+      card52: '../images/puke_52.jpg',
+      card53: '../images/puke_53.jpg',
+      card54: '../images/puke_54.jpg'
     }
 
     return imgs;
   },
+  /*
   preloadImgs: function () {//服务器图片预加载
     var count = 0;
     var that = this;
     var imgs = this.getImages();
     //var preLoadRes = [imgs.jd, imgs.jdWin, imgs.jdLose, imgs.st, imgs.stWin, imgs.stLose, imgs.bu, imgs.buWin, imgs.buLose];
-    var preLoadRes = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12",                           "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23",                            "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34",                            "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45",                            "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53", "card54"
-                  ];
+    var preLoadRes = [imgs.card1, imgs.card2, imgs.card3, imgs.card4, imgs.card5, imgs.card6, imgs.card7, imgs.card8, imgs.card9, imgs.card10, imgs.card11, imgs.card12, imgs.card13, imgs.card14, imgs.card15, imgs.card16, imgs.card17, imgs.card18, imgs.card19, imgs.card20, imgs.card21, imgs.card22, imgs.card23, imgs.card24, imgs.card25, imgs.card26, imgs.card27, imgs.card28, imgs.card29, imgs.card30, imgs.card31, imgs.card32, imgs.card33, imgs.card34, imgs.card35, imgs.card36, imgs.card37, imgs.card38, imgs.card39, imgs.card40, imgs.card41, imgs.card42, imgs.card43, imgs.card44, imgs.card45, imgs.card46, imgs.card47, imgs.card48, imgs.card49, imgs.card50, imgs.card51, imgs.card52, imgs.card53, imgs.card54
+    ];
     for (var i = 0; i < preLoadRes.length; i++) {
       wx.request({
         url: preLoadRes[i],
@@ -128,6 +129,7 @@ Page({
       });
     }
   },
+  */
   showStage: function () {
     this.setData({
       stageHidden: false,
@@ -205,8 +207,8 @@ Page({
   },
   setResultImg: function (state, player, pc1, pc2, pc3) {//设置比赛结果显示对应的输赢图片
     var imgs = this.getImages();
-    var imgName = ["card1", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12",                           "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23",                            "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34",                            "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45",                            "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53", "card54"
-                  ];
+    var imgName = ["card1", "card3", "card4", "card2", "card6", "card7", "card8", "card9", "card10", "card11", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45", "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53", "card54"
+    ];
     var playerImg = imgName[player - 1];
     var pcImg1 = imgName[pc1 - 1];
     var pcImg2 = imgName[pc2 - 1];
