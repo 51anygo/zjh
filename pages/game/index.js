08c41f21204224435c3ed8dcee85a8d5691c08b0
liyong335 <liyong6@jd.com>
2017-02-10 17:28:37 +0800

    liyong335__20170210

--- pages/game/index.js
+++ pages/game/index.js
@@ -207,7 +207,7 @@ Page({
   },
   setResultImg: function (state, player, pc1, pc2, pc3) {//设置比赛结果显示对应的输赢图片
     var imgs = this.getImages();
-    var imgName = ["card1", "card3", "card4", "card2", "card6", "card7", "card8", "card9", "card10", "card11", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45", "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53", "card54"
+    var imgName = ["card1", "card2", "card3", "card4", "card6", "card7", "card8", "card9", "card10", "card11", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27", "card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36", "card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45", "card46", "card47", "card48", "card49", "card50", "card51", "card52", "card53", "card54"
     ];
     var playerImg = imgName[player - 1];
     var pcImg1 = imgName[pc1 - 1];
